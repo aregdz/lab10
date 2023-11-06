f675e1f84017ee91fab25d3df6b60b8cc2300868
aregdz <144221910+aregdz@users.noreply.github.com>
2023-11-06 13:07:03 +0300

    s

---
+++ PyCharm/8task.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == "__main__":
+    input_string = input("Введите строку: ")
+    input_string = input_string.lower()
+    vowels = set("aeiou")
+    count = sum(1 for char in input_string if char in vowels)
+    print(f"Количество гласных в строке: {count}")
---
+++ PyCharm/individual.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == "__main__":
+    # Определим универсальное множество
+    u = set("abcdefghijklmnopqrstuvwxyz")
+    a = {"a", "d", "k", "l", "o", "s"}
+    b = {"d", "e", "k", "s", "u", "x"}
+    c = {"o", "p", "w"}
+    d = {"d", "n", "r", "y", "z"}
+    x = (a.difference(b)).union(c.intersection(d))
+    print(f"x = {x}")
+    da = u.difference(a)
+    db = u.difference(b)
+    y = (da.intersection(db)).difference(c.union(d))
+    print(f"y = {y}")
